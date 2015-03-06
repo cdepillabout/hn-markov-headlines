530e3fdfd6c325aa3b899d8087e1e2cae94833ed
(cdep)illabout <cdep.illabout@gmail.com>
2015-03-07 01:17:53 +0900

    Update

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,36 @@
 
 module Main where
 
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 import Web.HackerNews -- (getTopStories, hackerNews)
 
-main :: IO ()
-main = do
-    result <- hackerNews getTopStories
+(<<) :: Monad m => m a -> m b -> m a
+(<<) = flip (>>)
+
+getStoriesList :: MaybeT IO [Int]
+getStoriesList = do
+    lift $ putStrLn "Starting"
+    result <- lift $ hackerNews getTopStories
+    lift $ putStrLn "Got response from api..."
     case result of
-        Left err -> return () -- print err
-        Right (TopStories storiesList) -> return ()
+        Left _ -> fail "failed to get a response" -- MaybeT $ return Nothing
+        Right (TopStories storiesList) -> return storiesList
+
+downloadStory :: Int -> IO ()
+downloadStory storyInt = do
+    print =<< hackerNews (getStory (StoryId storyInt))
+
+
+downloadStories :: [Int] -> MaybeT IO ()
+downloadStories storiesList = do
+    lift $ forM_ (take 2 storiesList) downloadStory
+
+main :: IO ()
+main = void $ runMaybeT $ do
+    maybeStoriesList <- getStoriesList
+    lift $ print maybeStoriesList
+    downloadStories maybeStoriesList
+
 
