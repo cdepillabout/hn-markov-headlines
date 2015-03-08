13934cbec4d6d85feb16042ac7089843cfc2fdaa
(cdep)illabout <cdep.illabout@gmail.com>
2015-03-09 01:05:39 +0900

    Update

--- src/CreateMain.hs
+++ src/CreateMain.hs
@@ -12,57 +12,17 @@ import Data.Text.Encoding (encodeUtf8)
 import System.IO (hPutStrLn, stderr)
 import Web.HackerNews -- (getTopStories, hackerNews)
 
-getStoriesList :: ErrorT String IO [Int]
-getStoriesList = do
-    --lift $ putStrLn "Starting"
-    result <- lift $ hackerNews getTopStories
-    --lift $ putStrLn "Got response from api..."
-    case result of
-        Left _ -> fail "failed to get a response" -- MaybeT $ return Nothing
-        Right (TopStories storiesList) -> return storiesList
 
-downloadStory :: Int -> (Int, Int) -> ErrorT String IO (Int, Text)
-downloadStory tryNum@5 (storyInt, index) = do
-    let msg = "Warning: failed to get a response from downloadStory for story " ++
-                show storyInt ++ " at index " ++ show index ++
-                " after trying " ++ show tryNum ++ " times."
-    lift $ hPutStrLn stderr msg
-    fail msg
-downloadStory tryNum (storyInt, index) = do
-    -- lift $ putStrLn $ "Downloading story " ++ show index
-    storyEither <- lift $ hackerNews (getStory (StoryId storyInt))
-    case storyEither of
-        Left _ -> downloadStory (tryNum + 1) (storyInt, index)
-        Right story -> return (storyInt, storyTitle story)
-
-downloadStories :: [Int] -> IO [(Int, Text)]
-downloadStories storiesList = do
-        let storyWithIndexList = zip storiesList [1..]
-        let downloadedListErrorT = map (downloadStory 0)
-                                       -- (take 20 storyWithIndexList)
-                                       storyWithIndexList
-        downloadedList <- mapM runErrorT downloadedListErrorT
-        return $ rights downloadedList
-
-serialize :: [(Int, Text)] -> IO ()
-serialize titleList = do
-        let byteStringTitleList = map (fmap encodeUtf8) titleList
-        let encodedTitleList = encode byteStringTitleList
-        -- print titleList
-        BS.putStr encodedTitleList
-
-downloadAndSerialize :: IO (Either String ())
-downloadAndSerialize =
-    runErrorT $ do
-        storiesList <- getStoriesList
-        titleList <- lift $ downloadStories storiesList
-        lift $ serialize titleList
+makeSomeStories :: [(Int, Text)] -> IO ()
+makeSomeStories storiesList = print $ length storiesList
 
 main :: IO ()
-main = return ()
--- main = do
---     result <- downloadAndSerialize
---     case result of
---         Left err -> print $ "ERROR: " ++ show err
---         Right _ -> print "SUCCESS"
-
+main = do
+    serializedList <- BS.getContents
+    let decodedList = decode serializedList
+    case decodedList of
+        Left err ->
+            putStrLn $
+                "ERROR: Failed to decode the input string with the " ++
+                "following error: " ++ err
+        Right storiesList -> makeSomeStories $ map (fmap encodeUtf8) titleList
