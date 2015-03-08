a60bd56faeea1e72e98b9a56e8e8631ac5b53133
(cdep)illabout <cdep.illabout@gmail.com>
2015-03-09 01:26:05 +0900

    Added updated storieslist

--- Makefile
+++ Makefile
@@ -12,13 +12,13 @@ all:
 .cabal-sandbox/bin/hn-create-markov-headlines:
 	$(MAKE) install
 
-storyTitles:
+storiesList:
 	$(MAKE) download-headlines
 
 download-headlines: .cabal-sandbox/bin/hn-download-headlines
 	.cabal-sandbox/bin/hn-download-headlines > storiesList
 
-create-headlines: .cabal-sandbox/bin/hn-create-markov-headlines storyTitles
+create-headlines: .cabal-sandbox/bin/hn-create-markov-headlines storiesList
 	.cabal-sandbox/bin/hn-create-markov-headlines < storiesList
 
 install: .cabal-sandbox
--- src/CreateMain.hs
+++ src/CreateMain.hs
@@ -5,10 +5,10 @@ import Control.Monad.Trans.Class
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Either
-import Data.Serialize (encode)
+import Data.Serialize (decode)
 import Data.Text (Text)
 import qualified Data.Text as Text
-import Data.Text.Encoding (encodeUtf8)
+import Data.Text.Encoding (decodeUtf8)
 import System.IO (hPutStrLn, stderr)
 import Web.HackerNews -- (getTopStories, hackerNews)
 
@@ -25,4 +25,4 @@ main = do
             putStrLn $
                 "ERROR: Failed to decode the input string with the " ++
                 "following error: " ++ err
-        Right storiesList -> makeSomeStories $ map (fmap encodeUtf8) titleList
+        Right storiesList -> makeSomeStories $ map (fmap decodeUtf8) storiesList
